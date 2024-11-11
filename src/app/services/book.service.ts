69448f8af005bddfa2960b9d88a3093a5e82d713
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-10 21:01:37 -0300

    feat: add CRUD methods for managing books

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Book } from '../models/book';
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +9,25 @@ import { Injectable } from '@angular/core';
 export class BookService {
   private baseUrl = 'http://localhost:8080/api/book'; // Url con la que nos comunicaremos con el backend
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.baseUrl);
+  }
+
+  getBookById(id: number): Observable<Book> {
+    return this.http.get<Book>(`${this.baseUrl}/${id}`);
+  }
+
+  createBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.baseUrl, book);
+  }
+
+  updateBook(book: Book): Observable<Book> {
+    return this.http.put<Book>(this.baseUrl, book);
+  }
+
+  deleteBook(id: number) {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
 }
